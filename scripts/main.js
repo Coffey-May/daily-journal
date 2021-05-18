62f1cbc98ee14ecf6f4bed1b95653a444391e576
Coffey May <jcoffeymay6@gmail.com>
2021-05-18 14:57:33 -0500

    Full CRUD

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,14 @@
 import journalListComponent from "./journalListComponent.js"
 import EntryListComponent from "./JournalEntryList.js"
 import { journalhtml } from './journal.js'
-import { getEntries, saveEntries, deleteEntry, updateEntry } from './journalDataProvider.js'
-
-
-
-
-
+import { getEntries } from './journalDataProvider.js'
 
 getEntries()
-    
-    // .then(saveEntries)  
     .then(journalhtml)
     .then(journalListComponent)
-    .then(() => EntryListComponent() )
-    
-    
+    .then(() => EntryListComponent())
+
+
 
 
 
